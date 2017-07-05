6fe64b546858a31a01debc514f3be2cf270eaf9b
ITPuzkin <pwacc1996@mail.ru>
2017-07-05 21:59:41 +0300

    Initial commit

---
+++ src/main/java/MainServer.java
@@ -0,0 +1,12 @@
+import org.eclipse.jetty.server.Server;
+
+/**
+ * Created by qwerty on 05.07.2017.
+ */
+public class MainServer {
+
+    public static void main(String[] args) throws Exception{
+
+        Server server = new Server(8080);
+    }
+}
